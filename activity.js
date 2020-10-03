87c6fe385da9856482adf47c561d8919cee3ddca
Thomas <51972322+linn14@users.noreply.github.com>
2020-10-03 18:43:52 +0100

    V3

--- activity.js
+++ activity.js
@@ -1,14 +1,20 @@
 const SteamUser = require('steam-user');
 const client = new SteamUser({enablePicsCache: true});
-console.log("SteamRecentActivityHours Version 2");
-console.log("Logging in...");
+const config = require("./config.json");
+console.log("SteamRecentActivityHours Version 3");
 
 client.logOn({
-  accountName: "your username",
-  password: "your password",
+  accountName: (config.username),
+  password: (config.password),
 });
 
 client.on('appOwnershipCached', () => {
+  console.log(``);
+  console.log(`Account Name: ${client.accountInfo.name}`);
+  console.log(`VAC Ban Count: ${client.vac.numBans}`);
+  console.log(`Wallet Balance: ${client.wallet.balance}`);
+  console.log(``);
+  console.log(`Launching Games...`);
   var games = client.getOwnedApps();
   games[0] = 730;
   console.log(`Launched ${games.length} games!`);
