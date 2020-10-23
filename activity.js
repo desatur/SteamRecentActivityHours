b2612efb0a814754e619d66afa3e558e6577a97f
Thomas <51972322+linn14@users.noreply.github.com>
2020-10-23 22:10:09 +0100

    V5
    Added http server info, fixes, rewrite (Big Changes)

--- activity.js
+++ activity.js
@@ -1,9 +1,13 @@
 const SteamUser = require('steam-user');
+const http = require('http');
+// Loading up npm packages
 const client = new SteamUser({enablePicsCache: true});
 const config = require("./config.json");
-console.log("SteamRecentActivityHours Version 4-0.6");
-
-client.logOn({
+// Setting up
+var version = 5
+console.log(`SteamRecentActivityHours Version ${version}\nScript by: Aræon`);
+// Splash
+client.logOn({ // Logging in from config file
   accountName: (config.username),
   password: (config.password),
 });
@@ -13,6 +17,12 @@ client.on('appOwnershipCached', () => {
   console.log(`Launching Games...`);
   var games = client.getOwnedApps();
   games[0] = 730;
-  console.log(`Launched ${games.length} games and apps!`);
   client.gamesPlayed(games);
+  console.log(`Launched ${games.length} games and apps!\nSome apps or programs may not start, i have no clue how to fix this.`);
+  console.log(`\nLaunching http info server.`)
+  http.createServer(function (req, res) { // Start http server with information
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(`SteamRecentActivityHours Version ${version}\nScript by: Araeon\nRunning ${games.length} games and apps on ${client.accountInfo.name}.`);
+  }).listen(8080);
+  console.log(`Server started. Type http://localhost:8080/ in your browser to see how many games/apps are you playing!`)
 });
