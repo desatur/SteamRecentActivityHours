ae71cd393e2335871abadfaae631224f1d3093f8
Thomas <51972322+AtomSnow@users.noreply.github.com>
2021-04-01 14:15:00 +0200

    V8CLI

--- activity.js
+++ activity.js
@@ -1,16 +1,19 @@
 const SteamUser = require('steam-user');
-const http = require('http');
-// Loading up npm packages.
+// Loading up npm package.
+
 const client = new SteamUser({enablePicsCache: true});
 const config = require("./config.json");
-// Setting up.
-var version = 5
-console.log(`SteamRecentActivityHours Version ${version}\nScript by: Lycaoe`);
-// Splash
-client.logOn({ // Logging in from config file.
+// Call client.
+
+var version = 8CLI
+console.log(`SteamRecentActivityHours Version ${version}\nScript by: AtomSnow`);
+// Just splash text.
+
+client.logOn({ 
   accountName: (config.username),
   password: (config.password),
 });
+// Logging in from config file.
 
 client.on('appOwnershipCached', () => {
   console.log(`\nAccount Name: ${client.accountInfo.name}\nVAC Ban Count: ${client.vac.numBans}\nWallet Balance: ${client.wallet.balance}\n`);
@@ -19,10 +22,5 @@ client.on('appOwnershipCached', () => {
   games[0] = 730;
   client.gamesPlayed(games);
   console.log(`Launched ${games.length} games and apps!\nSome apps or programs may not start, i have no clue how to fix this.`);
-  console.log(`\nLaunching http info server.`)
-  http.createServer(function (req, res) { // Start http server with information (removing in version 6, bugged).
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(`SteamRecentActivityHours Version ${version}\nScript by: Araeon\nRunning ${games.length} games and apps on ${client.accountInfo.name}.`);
-  }).listen(8080);
-  console.log(`Server started. Type http://localhost:8080/ in your browser to see how many games/apps are you playing!`)
 });
+// Here, the magic begins, its not buggy anymore, enjoy simple code.
