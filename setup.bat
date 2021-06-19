920bbf3e5600924b889926e48ae2410503d02701
Thomas <51972322+AtomSnow@users.noreply.github.com>
2021-06-19 11:03:30 +0200

    Lurmog's Patch Improvement

--- setup.bat
+++ setup.bat
@@ -1,6 +1,13 @@
-echo off
+@echo off
 cls
 title SteamRecentActivityHours - Installing Packages
-@echo Installing packages, make sure you have node.js installed.
+echo Installing packages, make sure you have node.js installed.
+npm --version 1>NULL 2>&1
+if %ERRORLEVEL% NEQ 0 (
+echo You do not have npm installed on your system.
+exit
+) else goto next
+:next
 npm i steam-user
 pause
+exit
